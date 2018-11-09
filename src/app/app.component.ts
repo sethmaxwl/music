f50a58bbd5428f7366f9cab9f488b2bbbe2ff839
Seth <sethmaxwl@gmail.com>
2018-11-09 00:05:43 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { Howl, Howler } from 'howler';
+const { Howl, Howler } = require('howler');
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import * from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [NgbModule]
 })
 export class AppComponent {
   title = 'music';
+  playMusic() {
+    sound.play();
+  }
 }
+var sound = new Howl({
+  src: ['assets/bass.mp3'],
+  autoplay: false,
+  loop: false,
+  volume: 0.5
+});
