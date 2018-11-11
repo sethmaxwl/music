1d8ebb9b0c4b2e5ab8c3bac3c1244adf32ec5dea
Seth <sethmaxwl@gmail.com>
2018-11-10 19:35:33 -0500

    remove stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,6 @@ import { Howl, Howler } from 'howler';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import * from 'jquery';
 import * from 'tone';
-// var sound = new Howl({
-//   src: ['assets/bass.mp3'],
-//   autoplay: false,
-//   loop: false,
-//   html5: true,
-//   preload: true
-// });
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,42 +10,6 @@ import * from 'tone';
   imports: [NgbModule]
 })
 export class AppComponent {
-  title = 'music';
-  let octave = 5;
-  const keys = [];
-  let prevKey = 0;
-  const instruments = {
-    keyboard: {
-      a: 'Cl',
-      w: 'C#l',
-      s: 'Dl',
-      e: 'D#l',
-      d: 'El',
-      f: 'Fl',
-      t: 'F#l',
-      g: 'Gl',
-      y: 'G#l',
-      h: 'Al',
-      u: 'A#l',
-      j: 'Bl',
-      // Upper octave.
-      k: 'Cu',
-      o: 'C#u',
-      l: 'Du',
-      p: 'D#u',
-      ';': 'Eu',
-      "'": 'Fu',
-      ']': 'F#u',
-      '\\': 'Gu',
-    },
-  };
-  let instrument = Instruments.keyboard;
-  const keyToNote = function(key){
-    const note = instrument[key];
-    if (!note) {
-      return;
-    }
-    return Tone.Frequency(note.replace('l', octave).replace('u', octave + 1)).toNote();
-  };
-
+  title = "music";
+  
 }
