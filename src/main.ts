e208513576b8902a58e9fe475d20b37779fff31d
Seth <sethmaxwl@gmail.com>
2018-11-10 21:03:47 -0500

    setup material

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import 'hammerjs';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -10,4 +10,3 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
