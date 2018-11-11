2f7df118462b5e48f6d10fd111bb07d58586a168
Arkinion <nicholasgr44@gmail.com>
2018-11-10 19:33:24 -0500

    Removed the test sound stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,10 @@
 import { Component } from '@angular/core';
-import { Howl, Howler } from 'howler';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import * from 'jquery';
-import * from 'tone';
-// var sound = new Howl({
-//   src: ['assets/bass.mp3'],
-//   autoplay: false,
-//   loop: false,
-//   html5: true,
-//   preload: true
-// });
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  imports: [NgbModule]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'music';
-  let octave = 5;
-  const keys = [];
-  let prevKey = 0;
-  const instruments = {
-    keyboard: {
-      a: 'Cl',
-      w: 'C#l',
-      s: 'Dl',
-      e: 'D#l',
-      d: 'El',
-      f: 'Fl',
-      t: 'F#l',
-      g: 'Gl',
-      y: 'G#l',
-      h: 'Al',
-      u: 'A#l',
-      j: 'Bl',
-      // Upper octave.
-      k: 'Cu',
-      o: 'C#u',
-      l: 'Du',
-      p: 'D#u',
-      ';': 'Eu',
-      "'": 'Fu',
-      ']': 'F#u',
-      '\\': 'Gu',
-    },
-  };
-  let instrument = Instruments.keyboard;
-  const keyToNote = function(key){
-    const note = instrument[key];
-    if (!note) {
-      return;
-    }
-    return Tone.Frequency(note.replace('l', octave).replace('u', octave + 1)).toNote();
-  };
-
 }
